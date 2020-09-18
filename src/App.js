193feee238d7c00391f4b993e975389a4ba641f2
Carlos Pallares <blackparker77@gmail.com>
2020-09-17 21:20:01 -0500

    Incrementador Numerico

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    increment: 0
+  }
+  
+  incrementNumber = () => {
+    this.setState({
+      increment: this.state.increment + 1
+    });
+
+  }
+
   render() {
+    const { increment } = this.state;
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{ increment }</span>
+        <button id="inc" onClick={this.incrementNumber} >Incrementa</button>
       </div>
     );
   }
